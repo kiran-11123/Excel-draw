6f50e7bd8d822a57922acecd1ed8ff891a8005fb
Kiran <kiran.sadara6730@gmail.com>
2025-12-30 15:30:00 +0530

    prisma and auth routes are added

--- my-turborepo/apps/http-backend/src/controller/auth_controller.ts
+++ my-turborepo/apps/http-backend/src/controller/auth_controller.ts
@@ -1,12 +1,78 @@
 import { Request , Response } from "express"
-
+import { SigninService , SignupService } from "../services/auth_services"
 
 export const SigninController= async(req:Request, res:Response)=>{
      
     try{
 
+        const {email , password} = req.body;
+
+        if(!email || !password){
+            return res.status(404).json({
+                message : "Email or Password is Empty.."
+            })
+        }
+
+        const result = await SigninService(email  , password);
+
+        return res.status(200).json({
+            message : "User Logged in successfully",
+            token:result
+        })
+
     }
     catch(er){
+         const err = er as Error;
+        if(err.message === "Email not found"){
+            return res.status(400).json({
+                message : "Email not found"
+            })
+             
+        }
+        else if(err.message === "Password is Wrong"){
+              return res.status(400).json({
+                message : "Password Wrong"
+            })
+        }
+
+        return res.status(500).json({
+            message : 'Internal Server Error'
+        })
          
+    }
+}
+
+
+
+export const SignupController = async(req:Request , res:Response)=>{
+
+   try {
+        const { email, username, password } = req.body;
+
+        // Call service
+        const user = await SignupService(email, username, password);
+
+
+        return res.status(200).json({
+            message: "Registration Successful"
+        });
+    }
+    catch (err) {
+
+        const er = err as Error
+        
+         
+        if (er.message === "Email already registered") {
+            return res.status(400).json({ message: "Email already exists." });
+        }
+
+        if (er.message === "Username already taken") {
+            return res.status(400).json({ message: "Username already taken." });
+        }
+
+        return res.status(500).json({
+            message: "Internal Server Error",
+            error: er.message
+        });
     }
 }
--- my-turborepo/apps/http-backend/src/services/auth_services.ts
+++ my-turborepo/apps/http-backend/src/services/auth_services.ts
@@ -1,8 +1,32 @@
 import {JWT_SECRET} from '@repo/backend-common/config'
+import {prisma} from "@repo/db/prisma"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 export const SigninService = async(email:string , password:string)=>{
     
     try{
 
+        const find_user  =await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(!find_user){
+              throw new Error("Email not found")
+        }
+
+        const password_check = await bcrypt.compare(password , find_user.password);
+
+        if(!password_check){
+            throw new Error("Password is Wrong")
+        }
+       
+        const token_details = {user_id : find_user.userId , user_name : find_user.username , email  :find_user.email }
+
+        const token = jwt.sign(token_details , JWT_SECRET ,{expiresIn : "7d"})
+
+        return token 
 
 
 
@@ -11,4 +35,47 @@ export const SigninService = async(email:string , password:string)=>{
     catch(er){
          throw er;
     }
+}
+
+export const SignupService = async(email :string , username:string , password:string)=>{
+
+    try{
+
+        const find_user  =await prisma.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(find_user){
+              throw new Error("Email already registered")
+        }
+
+        const username_check = await prisma.user.findUnique({
+            where:{
+                username
+            }
+        })
+
+        if(username_check){
+             throw new Error("Username already taken")
+        }
+        const hash_password = await bcrypt.hash(password , 10);
+
+          await prisma.user.create({
+            data: {
+                email,
+                username,
+                password: hash_password
+            }
+        });
+
+    
+
+
+    }
+    catch(er){
+         throw er;
+    }
+        
 }
