2eae4557c4bbde342562523bae9529240e5295db
Kiran <kiran.sadara6730@gmail.com>
2025-12-31 13:37:31 +0530

    error while running backend

--- my-turborepo/apps/http-backend/src/controller/create_room_controller.ts
+++ my-turborepo/apps/http-backend/src/controller/create_room_controller.ts
@@ -1,36 +1,28 @@
-import { Response , Request } from "express";
-import { CreateRoomService } from "../services/create_room_services";
-import { AuthRequest } from "../middleware";
+import { Response  , Request} from "express";
 
-interface CreateRoomBody {
-  name: string;
-}
+import { CreateRoomService } from "../services/create_room_services";
 
 export const CreateRoomContoller = async (
   req: Request,
   res: Response
 ) => {
   try {
-    const { name } = req.body as CreateRoomBody;
+    const { name } = req.body;
+    // @ts-ignore
+    const user_id = req.user.user_id;
 
-    const user_id = (req as AuthRequest).user.user_id;
-
-    await CreateRoomService(name, user_id);
+   const result =  await CreateRoomService(name, user_id);
 
     return res.status(200).json({
-      message: "Room Created Successfully"
+      message: "Room Created Successfully",
     });
   } catch (er) {
     const err = er as Error;
 
     if (err.message === "Room Name is Already Taken") {
-      return res.status(400).json({
-        message: err.message
-      });
+      return res.status(400).json({ message: err.message });
     }
 
-    return res.status(500).json({
-      message: "Internal Server Error"
-    });
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
