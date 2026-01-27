9c56cc57735c5f82fda9db4f1a983611f1141e94
Kiran <kiran.sadara6730@gmail.com>
2026-01-27 19:37:50 +0530

    ws backend done

--- my-turborepo/apps/ws-backend/src/index.ts
+++ my-turborepo/apps/ws-backend/src/index.ts
@@ -4,6 +4,47 @@ import {JWT_SECRET} from "@repo/backend-common/config"
 const wss = new WebSocketServer({port : 8080})
 
 
+function checkUser(token : string):string | null{
+    const decoded = jwt.verify(token , JWT_SECRET);
+
+     if(typeof decoded === 'string'){
+        
+         return null;
+    }
+
+    if(!decoded || !(decoded as JwtPayload).user_id){
+       
+          return null;
+    }
+return decoded.user_id;
+     
+}
+
+//simplest to store the state in backend is global variable
+
+/*
+const users = [{
+
+    userId : 1,
+    rooms  :["room1" , "room2"] , 
+    ws : socket
+}
+] 
+
+*/
+
+interface User_interface{
+     
+    ws:WebSocket,
+    rooms :String[],
+    users : String,
+}
+
+
+
+const users: User_interface[]   = [];
+
+
 
 wss.on('connection ' , function connection(ws ,request){
       
@@ -14,17 +55,65 @@ wss.on('connection ' , function connection(ws ,request){
     const queryParams = new URLSearchParams(url.split('?')[1]); 
     const token = queryParams.get('token') || "";
 
-    const decoded  =jwt.verify(token , JWT_SECRET ) ; 
-
-    if(!decoded || !(decoded as JwtPayload).user_id){
-          ws.close();
-          return;
+    const decoded_user_id :string  | null  = checkUser(token);
+    
+    if(decoded_user_id ==null ){
+        ws.close();
+        return;
+       
     }
+
+    users.push({
+        users : decoded_user_id ,
+        rooms :[],
+        ws
+
+    })
+
+/*
+    message will be
+    {
+
+        type : "join_room",
+        roomId : "1" 
+    }
+
+    */
 
     ws.on("message" , function message( data:string){
        
-        if(data === "hi there"){
-            ws.send("Hello there")
+
+        const parsedData = JSON.parse(data as unknown as string);
+       
+        if(parsedData.type === 'join_room'){
+            const user = users.find(x => x.ws === ws);
+            user?.rooms.push(parsedData.roomId);
+        }
+
+
+        if(parsedData.type === 'leave_room'){
+            const user = users.find(x => x.ws === ws);
+            
+            if(!user){
+                return;
+            }
+            user.rooms = user?.rooms.filter(x => x === parsedData.room);
+        }
+
+        if(parsedData.type ==='chat'){
+             const roomId = parsedData.roomId;
+             const message   = parsedData.message;
+
+             users.forEach(user=>{
+                 if(user.rooms.includes(roomId)){
+                     user.ws.send(JSON.stringify({
+                        type:"chat",
+                        message : message ,
+                        roomId
+                     }))
+                 }
+             })
+
         }
     })
 
